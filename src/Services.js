0ba7cea6f79013de288e9a7a83b2691ff2b9724e
ismaeldcent04 <ismadadi.26@gmail.com>
2024-04-26 14:46:34 -0400

    Cambio de url de api y nominacenter app devExpress

--- src/Services.js
+++ src/Services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5443/api",
+    baseURL: "http://10.0.0.62:5443/api",
     headers: {
       "Content-Type": "application/json",
     },
